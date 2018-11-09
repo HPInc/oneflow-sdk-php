7b2f2688acef81b6d67671ccbbca38bc962e250d
Jose Andrés Puertas <joseandres.puertas@gmail.com>
2018-11-09 14:59:29 +0000

    feat: added shipment slaDays and item printQuantity properties

--- src/order/orderData/item/item.php
+++ src/order/orderData/item/item.php
@@ -23,141 +23,153 @@ class OneFlowItem extends OneFlowBase {
 		$this->__addProperty("sourceItemId", "", true);
 		$this->__addProperty("sku");
 		$this->__addProperty("quantity", 1, true);
+		$this->__addProperty("printQuantity");
 		$this->__addProperty("shrinkWrap", false);
 		$this->__addProperty("barcode", "", false);
 		$this->__addProperty("dispatchAlert");
 		$this->__addArray("extraData",null);
 
 		$this->__addProperty("colour");
-    }
+	}
 
-    /**
-     * newComponent function.
-     *
-     * @access public
-     * @param string $code
-     * @return OneFlowComponent
-     */
-    public function newComponent($code){
-    	$count = count($this->components);
-    	$component = new OneFlowComponent();
-        $component->setCode($code);
-	    $this->components[$count] = $component;
+	/**
+	 * newComponent function.
+	 *
+	 * @access public
+	 * @param string $code
+	 * @return OneFlowComponent
+	 */
+	public function newComponent($code){
+		$count = count($this->components);
+		$component = new OneFlowComponent();
+		$component->setCode($code);
+		$this->components[$count] = $component;
 
-	    return $this->components[$count];
-    }
+		return $this->components[$count];
+	}
 
-    /**
-     * addTextComponent function.
-     *
-     * @access public
-     * @return OneFlowComponent
-     */
-    public function addTextComponent()	{
-	    return $this->newComponent('text');
-    }
+	/**
+	 * addTextComponent function.
+	 *
+	 * @access public
+	 * @return OneFlowComponent
+	 */
+	public function addTextComponent()	{
+		return $this->newComponent('text');
+	}
 
-    /**
-     * addTextComponent function.
-     *
-     * @access public
-     * @param string $barcode
-     * @return string
-     */
-    public function setBarcode($barcode)	{
-	    return $this->barcode = $barcode;
-    }
+	/**
+	 * addTextComponent function.
+	 *
+	 * @access public
+	 * @param string $barcode
+	 * @return string
+	 */
+	public function setBarcode($barcode)	{
+		return $this->barcode = $barcode;
+	}
 
-    /**
-     * addCoverComponent function.
-     *
-     * @access public
-     * @return OneFlowComponent
-     */
-    public function addCoverComponent()	{
-	    return $this->newComponent('cover');
-    }
+	/**
+	 * addCoverComponent function.
+	 *
+	 * @access public
+	 * @return OneFlowComponent
+	 */
+	public function addCoverComponent()	{
+		return $this->newComponent('cover');
+	}
 
-    /**
-     * setDispatchAlert function.
-     *
-     * @access public
-     * @param mixed $dispatchAlert
-     * @return void
-     */
-    public function setDispatchAlert($dispatchAlert)	{
-	    $this->dispatchAlert = $dispatchAlert;
-    }
+	/**
+	 * setDispatchAlert function.
+	 *
+	 * @access public
+	 * @param mixed $dispatchAlert
+	 * @return void
+	 */
+	public function setDispatchAlert($dispatchAlert)	{
+		$this->dispatchAlert = $dispatchAlert;
+	}
 
-    /**
-     * setShipment function.
-     *
-     * @access public
-     * @param mixed $shipment
-     * @return void
-     */
-    public function setShipment($shipment)	{
-	    $this->shipmentIndex = $shipment->shipmentIndex;
-    }
+	/**
+	 * setShipment function.
+	 *
+	 * @access public
+	 * @param mixed $shipment
+	 * @return void
+	 */
+	public function setShipment($shipment)	{
+		$this->shipmentIndex = $shipment->shipmentIndex;
+	}
 
-    /**
-     * setBinding function.
-     *
-     * @access public
-     * @param mixed $binding
-     * @return boolean
-     */
-    public function setBinding($binding)	{
+	/**
+	 * setBinding function.
+	 *
+	 * @access public
+	 * @param mixed $binding
+	 * @return boolean
+	 */
+	public function setBinding($binding)	{
 		$this->__addList("binding", Array("perfect", "saddle", "wiro", "pur", "threadSawn"));
 		$this->binding = $binding;
 		return true;
-    }
+	}
 
-    /**
-     * setFolding function.
-     *
-     * @access public
-     * @param mixed $folding
-     * @return boolean
-     */
-    public function setFolding($folding)	{
+	/**
+	 * setFolding function.
+	 *
+	 * @access public
+	 * @param mixed $folding
+	 * @return boolean
+	 */
+	public function setFolding($folding)	{
 		$this->__addList("folding", Array("half", "gate", "cross", "concertina", "z", "2", "3"));
 		$this->folding = $folding;
 		return true;
-    }
+	}
 
-    /**
-     * setQuantity function.
-     *
-     * @access public
-     * @param mixed $quantity
-     * @return void
-     */
-    public function setQuantity($quantity)	{
-	    $this->quantity = $quantity;
-    }
+	/**
+	 * setQuantity function.
+	 *
+	 * @access public
+	 * @param mixed $quantity
+	 * @return void
+	 */
+	public function setQuantity($quantity)	{
+		$this->quantity = $quantity;
+	}
 
-    /**
-     * setSourceItemId function.
-     *
-     * @access public
-     * @param mixed $sourceItemId
-     * @return void
-     */
-    public function setSourceItemId($sourceItemId)	{
-	    $this->sourceItemId = $sourceItemId;
-    }
+	/**
+	 * setPrintQuantity function.
+	 *
+	 * @access public
+	 * @param integer $printQuantity
+	 * @return void
+	 */
+	public function setPrintQuantity($printQuantity)	{
+		$this->printQuantity = $printQuantity;
+	}
 
-    /**
-     * setSKU function.
-     *
-     * @access public
-     * @param mixed $skuCode
-     * @return void
-     */
-    public function setSKU($skuCode)	{
-	    $this->sku = $skuCode;
-    }
+	/**
+	 * setSourceItemId function.
+	 *
+	 * @access public
+	 * @param mixed $sourceItemId
+	 * @return void
+	 */
+	public function setSourceItemId($sourceItemId)	{
+		$this->sourceItemId = $sourceItemId;
+	}
+
+	/**
+	 * setSKU function.
+	 *
+	 * @access public
+	 * @param mixed $skuCode
+	 * @return void
+	 */
+	public function setSKU($skuCode)	{
+		$this->sku = $skuCode;
+	}
 
 	/**
 	 * setExtraData function.
